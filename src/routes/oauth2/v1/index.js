ee85960c65b760074d10b9e55728daee2673a8b9
Behnaz Nemati <behnaznemati2014@gmail.com>
2021-10-08 10:54:15 +0330

    fix: modify routes for swagger

--- src/routes/oauth2/v1/index.js
+++ src/routes/oauth2/v1/index.js
@@ -1,11 +1,15 @@
 const oauthCtrl = require('../../../services/oauth2-service');
 const { schemaTypes } = require('../../../lib');
 
+const swaggerTag = 'Authentication Service';
+
 module.exports = (fastify, options, next) => {
   // last step of authorization code which generates token. API will be called by client.
   // implicit, client_credentials and password types which provided token directly in one step. API will be called by user/client.
   fastify.post('/login', {
     schema: {
+      description: 'Login',
+      tags: [swaggerTag],
       body: {
         type: 'object',
         properties: {
